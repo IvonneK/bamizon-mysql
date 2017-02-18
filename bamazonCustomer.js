3ec22d797e14198e925da335f8c5670594d6902f
Ivonne Komis <ivonnekomis@Ivonnes-MBP.fios-router.home>
2017-02-18 02:38:08 -0500

    commit js logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,22 +8,48 @@ var connection = mysql.createConnection({
 	host		: 'localhost',
 	user		: 'root',
 	password	: 'root',
-	database	: ''
+	database	: 'bamazon_db',
+	PORT        :  8889,
 });
 
-connection.connect(function(err){
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadID);
-});
+// connection.connect(function(err){
+// 	if (err) {
+// 		console.error('error connecting: ' + err.stack);
+// 		return;
+// 	}
+// 	console.log('connected as id ' + connection.threadID);
+// });
 
 connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+ var table  = "products";
+
+function selectTable(table){
+	connection.query('SELECT * from ' + table, function (error, results, fields) {
+	  if (error) throw error;
+	  console.log('All records from products'); 
+	  console.log(results, '\n');
+	});
+};
+
+connection.query('SELECT * from products', function(error, results, fields)
+{
+	console.log(results);
+	console.log('\n');
+	console.log('before inquirer.prompt');
+
+	inquirer.prompt ([
+		{type: "input",
+			name: "product_id",
+			message: "Enter the product id you would like to purchase"
+		}	
+	]).then(function(data){
+		var product = data.product_id;
+		console.log('before query');
+		console.log('product', product)
+		connection.query('SELECT * from products where id=' + product, function(error, results, fields){
+			console.log(results);
+		});
+	});
 });
 
 
